9199bd3d57f187f8801ac651ed0d7d166fbb154f
giampieer <giampieer24@gmail.com>
2019-09-03 12:45:39 -0500

    prueba de hellosign

---
+++ index.php
@@ -0,0 +1,2 @@
+$client = new HelloSign\Client('e9341a24c808210ccb1cb14d04321c0e78d6d65da844843f946162446ea89a49');= new HelloSign\Client('e9341a24c808210ccb1cb14d04321c0e78d6d65da844843f946162446ea89a49');
+$signature_request = $client->getSignatureRequest('961f25267c4ee995619f1e28f0fcb28d2b68b84d');= $client->getSignatureRequest('961f25267c4ee995619f1e28f0fcb28d2b68b84d');
